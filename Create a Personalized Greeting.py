277cfb9611cf281c37a07ad19f2fa0271c8b776f
digitaldeepamjain <digitaldeepamjain@gmail.com>
2025-10-06 00:33:32 +0530

    this is my Assignment 1 : Basic python Concepts

---
+++ Create a Personalized Greeting.py
@@ -0,0 +1,4 @@
+nameF = input("Enter Your First Name : ");
+nameL = input("Enter Your Last Name : ");
+
+print("Hello, " + nameF + " " + nameL + "! Welcome to the Python program.");
---
+++ Perform Basic Mathematical Operations.py
@@ -0,0 +1,4 @@
+a = int(input("Enter The First Number : "))
+b = int(input("Enter The Second Number : "))
+
+print("Addition :" , (a + b), "\nSubtraction :" , (a - b), "\n Multiplication :" , (a * b), "\nDivision :" , (a / b))
